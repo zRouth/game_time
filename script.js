6577490eff0dacf0ec51cbcfac9bc920c3e41f65
zRouth <zrouthier@gmail.com>
2015-02-10 22:15:32 -0700

    added array as possible implementation of board

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
 $(function() {
 
  $('body').keyup(function(event){
+    // var board = { move: function(direction){ console.log(direction)} }
+    var board = new Board()
      var keyboardHandler = {
          38: function(){board.move('up')},
          40: function(){board.move('down')},
          37: function(){board.move('left')},
          39: function(){board.move('right')}
      };
-
+     console.log(event.which)
+     console.log(event.keyCode)
      if(keyboardHandler[event.which] !== undefined){
          keyboardHandler[event.keyCode]();
      }
  });
 });
 
-//==========================================================
 
-var Board = function(size) {
-    this.current_color = Board.BLACK;
-    this.size = size;
-    this.board = this.create_board(size);
-    this.last_move_passed = false;
-    this.in_atari = false;
-    this.attempted_suicide = false;
-};
-
-Board.EMPTY = 0;
-Board.BLACK = 1;
-Board.WHITE = 2;
-
-/*
- * Returns a size x size matrix with all entries initialized to Board.EMPTY
- */
-Board.prototype.create_board = function(size) {
-    var m = [];
-    for (var i = 0; i < size; i++) {
-        m[i] = [];
-        for (var j = 0; j < size; j++)
-            m[i][j] = Board.EMPTY;
-    }
-    return m;
-};
-
-
+var Board = function() {
+    this.cells = [
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+    ]
+    this.move = function(direction){ console.log(direction) }
+}
