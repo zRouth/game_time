6d9975385490ab6d28a5433c6497db394a7c473c
ChasevanHekken <cvh1717@gmail.com>
2015-02-18 13:24:58 -0700

    built functions and got left right functionality for push

--- prac.js
+++ prac.js
@@ -1,35 +1,72 @@
-var arr = [8,16,8,8];
+var singleArray = function(arr) {
+  var new_arr = [];
 
-var new_arr = [];
+  for(var i=0; i < arr.length; i++) {
+    if (arr[i] !== null) {
+        new_arr.push(arr[i])
+      }
+    }
 
-for(var i=0; i < arr.length; i++) {
-  if (arr[i] !== null) {
-    new_arr.push(arr[i])
+  var previous_value = 0;
+  var bool_switch = false;
+  var newer_arr = [];
+
+  for(var i=0; i < new_arr.length; i++) {
+    newer_arr.push(new_arr[i]);
+
+    if (new_arr[i] == previous_value) {
+      newer_arr.pop();
+      newer_arr.pop();
+      newer_arr.push(new_arr[i] * 2);
+      previous_value = 0;
+      bool_switch = true;
+    }
+
+    if (bool_switch == false) {
+      previous_value = new_arr[i];
+    }
+    bool_switch = false;
   }
+
+  return newer_arr;
 };
 
-var previous_value = 0;
 
-var bool_switch = false;
+var parseGrid = function(grid, direction) {
+  var new_grid = [];
+  var temp = [];
 
-var newer_arr = [];
-
-for(var i=0; i < new_arr.length; i++) {
-  newer_arr.push(new_arr[i]);
-
-  if (new_arr[i] == previous_value) {
-    newer_arr.pop();
-    newer_arr.pop();
-    newer_arr.push(new_arr[i] * 2);
-    previous_value = 0;
-    bool_switch = true;
+  if (direction === "right" || direction === "left") {
+    for(var i=0; i < grid.length; i++) {
+      temp = singleArray(grid[i])
+      while (temp.length < 5) {
+        temp.push(null);
+      }
+      new_grid.push(temp);
+    }
   }
+    return new_grid;
 
-  if (bool_switch == false) {
-    previous_value = new_arr[i];
-  }
 
-  bool_switch = false;
 };
 
-console.log(newer_arr);
+// console.log(singleArray([8,16,16,8]));
+
+
+
+var gameGrid = [
+  [8,16,16,8],
+  [4,4,4,null],
+  [8,8,8,null],
+  [16,null,16,null]
+];
+
+console.log(parseGrid(gameGrid, "right"));
+
+// for(var i=0; i < grid.length; i++) {
+//   for(var j=0; j < grid[i].length; j++) {
+//
+//     console.log(grid[i][j]);
+//
+//   }
+// }
