304ce9d0f629103b8ed0f665c5ced8eae75c8e14
ChasevanHekken <cvh1717@gmail.com>
2015-02-17 20:46:15 -0700

    one 4 item array works

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ $(function() {
     // var board = { move: function(direction){ console.log(direction)} }
     var board = new Board()
      var keyboardHandler = {
-         38: function(){board.move('up')},
+         38: function(){ $('.grid-cell').toggleClass('turn-black')},
          40: function(){board.move('down')},
          37: function(){board.move('left')},
          39: function(){board.move('right')}
      };
-     console.log(event.which)
-     console.log(event.keyCode)
      if(keyboardHandler[event.which] !== undefined){
          keyboardHandler[event.keyCode]();
      }
@@ -18,12 +16,21 @@ $(function() {
 });
 
 
-var Board = function() {
-    this.cells = [
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-    ]
-    this.move = function(direction){ console.log(direction) }
-}
+// var Board = function() {
+//     this.cells = [
+//         [0,0,0,0],
+//         [0,0,0,0],
+//         [0,0,0,0],
+//         [0,0,0,0],
+//     ]
+//     this.move = function(direction){ console.log(direction) }
+// }
+
+// function shiftRight(row) {
+//   return row.reduce(function) (array, currentElement, currentIndex, originalArray) {
+//     if (!originalArray[currentIndex + 1]) {
+//       array[currentIndex + 1] = currentElement;
+//     }
+//     return array;
+//   }, [null, null, null, null]);
+// }
